eb842a4b8138a076f839f4f96033a85eb44a4c25
lexmin0412 <zhangle_dev@outlook.com>
2022-09-03 18:32:46 +0800

    chore: use pnpm

--- src/config/logs.ts
+++ src/config/logs.ts
@@ -4,8 +4,8 @@
  */
 export const log_default = (params: any) => {
 	const { PKG_NAME } = params
-	console.log('模版初始化成功✅');
-	console.log(`$ cd ${PKG_NAME} 进入文件夹`);
+	console.log('模版初始化成功✅')
+	console.log(`$ cd ${PKG_NAME} 进入文件夹`)
 }
 
 /**
@@ -16,8 +16,8 @@ export const log_lib_rollup_ts = (params: any) => {
 	const { PKG_NAME } = params
 	console.log(`模版初始化成功，请执行一下命令手动安装依赖并进行调试：
 $ cd ${PKG_NAME} 进入文件夹
-$ yarn 安装依赖
-$ yarn build 编译
+$ pnpm i 安装依赖
+$ pnpm build 编译
 					`)
 }
 
@@ -25,12 +25,12 @@ $ yarn build 编译
  * 结束时的统一log
  * @param params
  */
-export const logsEnd = (params) => {
+export const logsEnd = params => {
 	const { PKG_TYPE } = params
 	switch (PKG_TYPE) {
 		case 'ROLLUP_TS':
 			log_lib_rollup_ts(params)
-			break;
+			break
 		default:
 			log_default(params)
 	}
